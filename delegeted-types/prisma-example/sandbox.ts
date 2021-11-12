8ebaed0a8b2195a7bacc65e624cd575186b7833c
ruheni <aleruheni@outlook.com>
2021-11-12 15:48:43 +0300

    add prisma  delegated types example

---
+++ delegeted-types/prisma-example/sandbox.ts
@@ -0,0 +1,64 @@
+import { PrismaClient } from "@prisma/client";
+import { v4 as uuid } from 'uuid'
+
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const articleId = uuid()
+  await prisma.$transaction([
+    prisma.article.create({ data: { id: articleId, title: 'Delegated Types?' } }),
+    prisma.content.create({ data: { id: articleId, type: 'article' } })
+  ])
+
+  const videoId = uuid()
+  await prisma.$transaction([
+    prisma.video.create({
+      data: {
+        id: videoId,
+        title: 'What is Prisma',
+        duration: 68,
+        url: 'https://youtu.be/EEDGwLB55bI'
+      }
+    }),
+    prisma.content.create({ data: { id: videoId, type: 'video' } })
+  ])
+
+
+  const imageId = uuid()
+  await prisma.$transaction([
+    prisma.image.create({
+      data: {
+        id: imageId,
+        width: 100,
+        height: 100,
+        fileName: 'Prisma Logo',
+        url: 'https://pbs.twimg.com/profile_images/1454143920414285824/UA64pizS_400x400.jpg'
+      }
+    }),
+    prisma.content.create({ data: { id: imageId, type: 'image' } })
+  ])
+
+  const content = await prisma.content.findMany({
+    orderBy: {
+      type: 'asc'
+    }
+  });
+  
+  const feed = await Promise.all(content.map((contentItem) => {
+    // @ts-ignore
+    return prisma[contentItem.type].findUnique({ where: { id: contentItem.id } });
+  }))
+
+  console.dir(feed, { depth: Infinity })
+
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
